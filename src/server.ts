58972264f4c48e8c881d2ed308f380efeeca1b93
QiroNT <i@ntz.im>
2022-01-24 13:09:20 +0800

    refactor: wait for all images in card

--- src/server.ts
+++ src/server.ts
@@ -48,6 +48,19 @@ async function buildDriver() {
   return driver;
 }
 
+async function buildDriverLocal() {
+  const driver = await new Builder().forBrowser("firefox").build();
+
+  await driver.manage().window().setRect({
+    x: 0,
+    y: 0,
+    width: 1024,
+    height: 768,
+  });
+
+  return driver;
+}
+
 async function process(did: string): Promise<Uint8Array> {
   const driver = await buildDriver();
   const log = (t: string) => {
@@ -70,76 +83,80 @@ async function process(did: string): Promise<Uint8Array> {
     log("avatar located");
 
     // wait until all images are loaded
-    await driver.executeAsyncScript(function () {
-      const did: string = arguments[0];
-      const callback = arguments[arguments.length - 1];
-      const images = Array.from(
-        document.querySelectorAll<HTMLImageElement>(".img-content")
-      );
+    await driver.executeAsyncScript(
+      function () {
+        // const did: string = arguments[0];
+        const card: HTMLDivElement = arguments[1];
+        const callback = arguments[arguments.length - 1];
+        const images = Array.from(
+          card.querySelectorAll<HTMLImageElement>("img")
+        );
+        console.log(images);
 
-      images.push(
-        document.querySelector<HTMLLinkElement>(`#dynamicId_${did}`)!
-          .children[0].children[0] as HTMLImageElement
-      );
+        const buttonBar = document.querySelector(
+          ".card .main-content .button-bar"
+        );
+        buttonBar?.childNodes.forEach((node) => {
+          const span = (node as HTMLDivElement).children[0] as HTMLElement;
+          if (span.tagName !== "SPAN") return;
 
-      const buttonBar = document.querySelector(
-        ".card .main-content .button-bar"
-      );
-      buttonBar?.childNodes.forEach((node) => {
-        const span = (node as HTMLDivElement).children[0] as HTMLElement;
-        if (span.tagName !== "SPAN") return;
+          const i = span.children[0] as HTMLElement;
+          const uri = window
+            .getComputedStyle(i)
+            .getPropertyValue("background-image");
+          const url = uri.slice(5, -2);
 
-        const i = span.children[0] as HTMLElement;
-        const uri = window
-          .getComputedStyle(i)
-          .getPropertyValue("background-image");
-        const url = uri.slice(5, -2);
+          const img = new Image();
+          img.src = url;
+          images.push(img);
+        });
 
-        const img = new Image();
-        img.src = url;
-        images.push(img);
-      });
-
-      let loaded = 0;
-      const finishOne = () => {
-        loaded++;
-        if (loaded === images.length) callback();
-      };
-      images.forEach((img) => {
-        if (img.tagName !== "IMG") return finishOne();
-        if (img.complete) finishOne();
-        else img.addEventListener("load", finishOne);
-      });
-    }, did);
+        let loaded = 0;
+        const finishOne = () => {
+          loaded++;
+          if (loaded === images.length) callback();
+        };
+        images.forEach((img) => {
+          if (img.tagName !== "IMG") return finishOne();
+          if (img.complete) finishOne();
+          else {
+            img.addEventListener("load", finishOne);
+            img.addEventListener("error", finishOne);
+          }
+        });
+      },
+      did,
+      card
+    );
     log("images loaded");
 
     await driver.executeScript(function () {
-      // make comment box invisible
       const card: HTMLDivElement = arguments[0];
+      // clear border
+      card.style.border = "none";
+      // make comment box invisible
       const commentbox = card.querySelector<HTMLDivElement>(".panel-area")!;
       commentbox.style.display = "none";
+      // make share button invisible
       const bottonAera = card.querySelector<HTMLDivElement>(".button-area")!;
       bottonAera.style.display = "none";
 
       // make login box invisible
-      const popovers = document.querySelectorAll<HTMLDivElement>(
-        ".van-popover.van-popper"
-      );
-      popovers.forEach((popover) => {
-        popover.style.display = "none";
-      });
+      const style = document.createElement("style");
+      style.innerHTML = `
+        .van-popover.van-popper {
+          display: none !important;
+        }
+        .login-tip {
+          display: none !important;
+        }
+      `;
+      document.head.appendChild(style);
 
       // scale up
       document.body.style.scale = "1.5";
     }, card);
     log("ready to take screenshot");
-
-    // await driver.wait(
-    //   until.elementIsNotVisible(
-    //     driver.findElement(By.css(".van-popover.van-popper"))
-    //   ),
-    //   5000
-    // );
 
     const screenshot = await card.takeScreenshot();
     log("screenshot taken");
