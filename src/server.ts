0ee41629d43d2871f0f5c0ce42249a629461d67d
QiroNT <i@ntz.im>
2022-04-03 15:28:16 +0800

    fix: wait for main image

--- src/server.ts
+++ src/server.ts
@@ -1,148 +1,131 @@
-import "cross-fetch/polyfill";
-import { Builder, By, Capabilities, until } from "selenium-webdriver";
-import * as grpc from "@grpc/grpc-js";
-import {
-  IScreenshotServer,
-  ScreenshotService,
-} from "../protos/protos/grpc_grpc_pb";
-import { ScreenshotResult } from "../protos/protos/grpc_pb";
+import 'cross-fetch/polyfill'
+import { Builder, By, Capabilities, until } from 'selenium-webdriver'
+import * as grpc from '@grpc/grpc-js'
+import { IScreenshotServer, ScreenshotService } from '../protos/protos/grpc_grpc_pb'
+import { ScreenshotResult } from '../protos/protos/grpc_pb'
 
-const capabilities = Capabilities.firefox();
+const capabilities = Capabilities.firefox()
 
 async function buildDriver() {
   while (true) {
     try {
-      const res = await fetch("http://selenium:4444/wd/hub/status");
+      const res = await fetch('http://selenium:4444/wd/hub/status')
       if (!res) {
-        throw undefined;
+        throw undefined
       }
-      const red = await res.json();
+      const red = await res.json()
       if (!red.value.ready) {
-        console.log(red.value.message);
-        console.log(JSON.stringify(red.value));
-        throw undefined;
+        console.log(red.value.message)
+        console.log(JSON.stringify(red.value))
+        throw undefined
       }
-      break;
+      break
     } catch (e) {
-      console.log("selenium is not ready, retry in 1s");
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log('selenium is not ready, retry in 1s')
+      await new Promise((resolve) => setTimeout(resolve, 1000))
     }
   }
 
-  console.log("connecting to driver");
+  console.log('connecting to driver')
 
-  const driver = await new Builder()
-    .usingServer("http://selenium:4444/wd/hub")
-    .withCapabilities(capabilities)
-    .build();
+  const driver = await new Builder().usingServer('http://selenium:4444/wd/hub').withCapabilities(capabilities).build()
 
   await driver.manage().window().setRect({
     x: 0,
     y: 0,
     width: 1024,
     height: 768,
-  });
+  })
 
-  console.log("driver is now available");
+  console.log('driver is now available')
 
-  return driver;
+  return driver
 }
 
 async function buildDriverLocal() {
-  const driver = await new Builder().forBrowser("firefox").build();
+  const driver = await new Builder().forBrowser('firefox').build()
 
   await driver.manage().window().setRect({
     x: 0,
     y: 0,
     width: 1024,
     height: 768,
-  });
+  })
 
-  return driver;
+  return driver
 }
 
 async function process(did: string): Promise<Uint8Array> {
-  const driver = await buildDriver();
+  const driver = await buildDriver()
   const log = (t: string) => {
-    console.log(`[${did}] ${t}`);
-  };
+    console.log(`[${did}] ${t}`)
+  }
   try {
-    log("navigating");
-    await driver.get(`https://t.bilibili.com/${did}?tab=3`);
-    log("page loaded");
+    log('navigating')
+    await driver.get(`https://t.bilibili.com/${did}?tab=3`)
+    log('page loaded')
 
     // wait until card is loaded
-    const card = await driver.wait(
-      until.elementLocated(By.css(`.card[data-did="${did}"]`)),
-      5000
-    );
-    log("card loaded");
+    const card = await driver.wait(until.elementLocated(By.css(`.card[data-did="${did}"]`)), 5000)
+    log('card loaded')
 
     // wait until avatar is located
-    await driver.wait(until.elementLocated(By.css(`#dynamicId_${did}`)), 2000);
-    log("avatar located");
+    await driver.wait(until.elementLocated(By.css(`#dynamicId_${did}`)), 2000)
+    log('avatar located')
 
     // wait until all images are loaded
     await driver.executeAsyncScript(
       function () {
         // const did: string = arguments[0];
-        const card: HTMLDivElement = arguments[1];
-        const callback = arguments[arguments.length - 1];
-        const images = Array.from(
-          card.querySelectorAll<HTMLImageElement>("img")
-        );
-        console.log(images);
+        const card: HTMLDivElement = arguments[1]
+        const callback = arguments[arguments.length - 1]
+        const images = Array.from(card.querySelectorAll<HTMLImageElement>('img'))
 
-        const buttonBar = document.querySelector(
-          ".card .main-content .button-bar"
-        );
-        buttonBar?.childNodes.forEach((node) => {
-          const span = (node as HTMLDivElement).children[0] as HTMLElement;
-          if (span.tagName !== "SPAN") return;
+        const bgImages = document.querySelectorAll('.card .main-content .button-bar span > i, .card .main-content .post-content .imagesbox .card div.img-content')
+        bgImages.forEach((node) => {
+          const uri = window.getComputedStyle(node).getPropertyValue('background-image')
+          if (!uri.startsWith('url(')) return
+          const url = uri.slice(5, -2)
 
-          const i = span.children[0] as HTMLElement;
-          const uri = window
-            .getComputedStyle(i)
-            .getPropertyValue("background-image");
-          const url = uri.slice(5, -2);
+          const img = new Image()
+          img.src = url
+          images.push(img)
+        })
 
-          const img = new Image();
-          img.src = url;
-          images.push(img);
-        });
+        console.log(images)
 
-        let loaded = 0;
+        let loaded = 0
         const finishOne = () => {
-          loaded++;
-          if (loaded === images.length) callback();
-        };
+          loaded++
+          if (loaded === images.length) callback()
+        }
         images.forEach((img) => {
-          if (img.tagName !== "IMG") return finishOne();
-          if (img.complete) finishOne();
+          if (img.tagName !== 'IMG') return finishOne()
+          if (img.complete) finishOne()
           else {
-            img.addEventListener("load", finishOne);
-            img.addEventListener("error", finishOne);
+            img.addEventListener('load', finishOne)
+            img.addEventListener('error', finishOne)
           }
-        });
+        })
       },
       did,
-      card
-    );
-    log("images loaded");
+      card,
+    )
+    log('images loaded')
 
     await driver.executeScript(function () {
-      const card: HTMLDivElement = arguments[0];
+      const card: HTMLDivElement = arguments[0]
       // clear border
-      card.style.border = "none";
+      card.style.border = 'none'
       // make comment box invisible
-      const commentbox = card.querySelector<HTMLDivElement>(".panel-area")!;
-      commentbox.style.display = "none";
+      const commentbox = card.querySelector<HTMLDivElement>('.panel-area')!
+      commentbox.style.display = 'none'
       // make share button invisible
-      const bottonAera = card.querySelector<HTMLDivElement>(".button-area")!;
-      bottonAera.style.display = "none";
+      const bottonAera = card.querySelector<HTMLDivElement>('.button-area')!
+      bottonAera.style.display = 'none'
 
       // make login box invisible
-      const style = document.createElement("style");
+      const style = document.createElement('style')
       style.innerHTML = `
         .van-popover.van-popper {
           display: none !important;
@@ -150,50 +133,46 @@ async function process(did: string): Promise<Uint8Array> {
         .login-tip {
           display: none !important;
         }
-      `;
-      document.head.appendChild(style);
+      `
+      document.head.appendChild(style)
 
       // scale up
-      document.body.style.scale = "1.5";
-    }, card);
-    log("ready to take screenshot");
+      document.body.style.scale = '1.5'
+    }, card)
+    log('ready to take screenshot')
 
-    const screenshot = await card.takeScreenshot();
-    log("screenshot taken");
+    const screenshot = await card.takeScreenshot()
+    log('screenshot taken')
 
-    return Buffer.from(screenshot, "base64");
+    return Buffer.from(screenshot, 'base64')
   } finally {
-    await driver.quit();
-    log("driver cleaned up");
+    await driver.quit()
+    log('driver cleaned up')
   }
 }
 
-let queue = Promise.resolve();
+let queue = Promise.resolve()
 
-const server = new grpc.Server();
+const server = new grpc.Server()
 
 const screenshotService: IScreenshotServer = {
   shotBld(call, callback) {
     queue = queue.then(async () => {
-      let res: Uint8Array;
+      let res: Uint8Array
       try {
-        res = await process(call.request.getDynamicid());
+        res = await process(call.request.getDynamicid())
       } catch (e) {
-        console.log(`[${call.request.getDynamicid()}] failed with error: `, e);
-        res = Buffer.from([]);
+        console.log(`[${call.request.getDynamicid()}] failed with error: `, e)
+        res = Buffer.from([])
       }
-      const msg = new ScreenshotResult();
-      msg.setPngimage(res);
-      callback(null, msg);
-    });
+      const msg = new ScreenshotResult()
+      msg.setPngimage(res)
+      callback(null, msg)
+    })
   },
-};
-server.addService(ScreenshotService, screenshotService);
-server.bindAsync(
-  "0.0.0.0:3000",
-  grpc.ServerCredentials.createInsecure(),
-  () => {
-    server.start();
-    console.log("Server listening on 0.0.0.0:3000");
-  }
-);
+}
+server.addService(ScreenshotService, screenshotService)
+server.bindAsync('0.0.0.0:3000', grpc.ServerCredentials.createInsecure(), () => {
+  server.start()
+  console.log('Server listening on 0.0.0.0:3000')
+})
